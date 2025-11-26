d112d16786f2d2c77322c6c37d5a7ca2ddecf295
Shika <greffinroid@ymail.com>
2025-11-27 02:02:18 +0900

    Add print header to macros header

--- include/macros.h
+++ include/macros.h
@@ -1,7 +1,14 @@
 #pragma once
 
 #ifndef NDEBUG
+#include <print>
 #define DLOG(x, ...) std::println(x __VA_OPT__(, ) __VA_ARGS__)
+
+inline void _keep_print_include() {
+	if (false) {
+		std::println();
+	}
+}
 #else
 #define DLOG(x)
 #endif // DEBUG
